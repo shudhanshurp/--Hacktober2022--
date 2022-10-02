616362bfc3b89ce3c894d684992272f5aaf1571a
Neel Patel <89609891+Neel2107@users.noreply.github.com>
2022-10-02 11:53:02 +0530

    Update app.js

--- ParaGen/setup/app.js
+++ ParaGen/setup/app.js
@@ -21,15 +21,16 @@ form.addEventListener("submit", function (e) {
     const value = parseInt(amount.value);
     const random = Math.floor(Math.random() * text.length);
 
-    if (isNaN(value) && value > 9) {
-        result.innerHTML = `<p class="result">${text[random]}</p>`;
-    } else {
-        let tempText = text.slice(1, value);
-        tempText = tempText
-            .map(function (item) {
-                return `<p class="result">${item}</p>`;
-            })
-            .join("");
-        result.innerHTML = tempText;
+  if (isNaN(value) || value < 0 || value > 9) {
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText
+      .map(function (item) {
+        return `<p class="result">${item}</p>`;
+      })
+      .join("");
+    result.innerHTML = tempText;
+  }
     }
 });
